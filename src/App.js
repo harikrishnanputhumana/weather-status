ad13eaa56daf6e2c7b762926a7f4d418b404e40c
Harikrishnan <hari@Harikrishnans-MacBook-Air.local>
2022-03-18 20:57:10 +0530

    api integrated and value loaded from api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import React, { useState } from 'react';
 import './App.css';
-
+import axios from 'axios'
+import { API_KEY } from './constants/constants';
 function App() {
-  return (
-    <div className="App">
-      <h1>test</h1>
+  const [temp,setTemp]=useState()
+  function test(){
+    axios.get(`http://api.weatherstack.com/current?access_key=${API_KEY}&query=delhi`).then((response)=>{
+      console.log(response);
+      setTemp(response.data.current.temperature)
+    })
+  }
+    return(
+      <div className="App">
+      <button onClick={test}>click</button>
+      <h1>{temp}</h1>
+
+
     </div>
-  );
+    )
+    
+  
 }
 
 export default App;
---
+++ src/constants/constants.js
@@ -0,0 +1,2 @@
+export const baseUrl="http://api.weatherstack.com/current?access_key";
+export const API_KEY="6553f5e61566cc25b1992311eef3080e";
